530c85c1addf1b8a03eb387b92b80dec8059b1dc
Daniel Rumfelt <76234995+GCUStudent-DanielRumfelt@users.noreply.github.com>
2022-08-11 23:40:27 -0400

    Add files via upload

---
+++ Week_6_Activity_13/Week_6_Activity_13/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Week_6_Activity_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.startTurn = new System.Windows.Forms.Button();
+            this.close = new System.Windows.Forms.Button();
+            this.gameBox00 = new System.Windows.Forms.Label();
+            this.gameBox01 = new System.Windows.Forms.Label();
+            this.gameBox03 = new System.Windows.Forms.Label();
+            this.gameBox10 = new System.Windows.Forms.Label();
+            this.gameBox11 = new System.Windows.Forms.Label();
+            this.gameBox12 = new System.Windows.Forms.Label();
+            this.gameBox20 = new System.Windows.Forms.Label();
+            this.gameBox22 = new System.Windows.Forms.Label();
+            this.gameBox23 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(9, 84);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 254);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // startTurn
+            // 
+            this.startTurn.BackColor = System.Drawing.Color.Yellow;
+            this.startTurn.ForeColor = System.Drawing.Color.Black;
+            this.startTurn.Location = new System.Drawing.Point(9, 137);
+            this.startTurn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.startTurn.Name = "startTurn";
+            this.startTurn.Size = new System.Drawing.Size(66, 48);
+            this.startTurn.TabIndex = 1;
+            this.startTurn.Text = "Start Turn";
+            this.startTurn.UseVisualStyleBackColor = false;
+            this.startTurn.Click += new System.EventHandler(this.startTurn_Click);
+            // 
+            // close
+            // 
+            this.close.BackColor = System.Drawing.Color.Yellow;
+            this.close.Location = new System.Drawing.Point(9, 194);
+            this.close.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(66, 48);
+            this.close.TabIndex = 2;
+            this.close.Text = "Close Game";
+            this.close.UseVisualStyleBackColor = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // gameBox00
+            // 
+            this.gameBox00.AutoSize = true;
+            this.gameBox00.Font = new System.Drawing.Font("NCAA Clemson Paw Hammer", 63F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameBox00.Location = new System.Drawing.Point(112, 93);
+            this.gameBox00.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.gameBox00.Name = "gameBox00";
+            this.gameBox00.Size = new System.Drawing.Size(0, 70);
+            this.gameBox00.TabIndex = 3;
+            // 
+            // gameBox01
+            // 
+            this.gameBox01.AutoSize = true;
+            this.gameBox01.Font = new System.Drawing.Font("NCAA Clemson Paw Hammer", 63F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameBox01.Location = new System.Drawing.Point(240, 93);
+            this.gameBox01.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.gameBox01.Name = "gameBox01";
+            this.gameBox01.Size = new System.Drawing.Size(0, 70);
+            this.gameBox01.TabIndex = 4;
+            // 
+            // gameBox03
+            // 
+            this.gameBox03.AutoSize = true;
+            this.gameBox03.Font = new System.Drawing.Font("NCAA Clemson Paw Hammer", 63F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameBox03.Location = new System.Drawing.Point(376, 93);
+            this.gameBox03.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.gameBox03.Name = "gameBox03";
+            this.gameBox03.Size = new System.Drawing.Size(0, 70);
+            this.gameBox03.TabIndex = 5;
+            // 
+            // gameBox10
+            // 
+            this.gameBox10.AutoSize = true;
+            this.gameBox10.Font = new System.Drawing.Font("NCAA Clemson Paw Hammer", 63F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameBox10.Location = new System.Drawing.Point(112, 194);
+            this.gameBox10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.gameBox10.Name = "gameBox10";
+            this.gameBox10.Size = new System.Drawing.Size(0, 70);
+            this.gameBox10.TabIndex = 6;
+            // 
+            // gameBox11
+            // 
+            this.gameBox11.AutoSize = true;
+            this.gameBox11.Font = new System.Drawing.Font("NCAA Clemson Paw Hammer", 63F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameBox11.Location = new System.Drawing.Point(240, 194);
+            this.gameBox11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.gameBox11.Name = "gameBox11";
+            this.gameBox11.Size = new System.Drawing.Size(0, 70);
+            this.gameBox11.TabIndex = 7;
+            // 
+            // gameBox12
+            // 
+            this.gameBox12.AutoSize = true;
+            this.gameBox12.Font = new System.Drawing.Font("NCAA Clemson Paw Hammer", 63F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameBox12.Location = new System.Drawing.Point(376, 194);
+            this.gameBox12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.gameBox12.Name = "gameBox12";
+            this.gameBox12.Size = new System.Drawing.Size(0, 70);
+            this.gameBox12.TabIndex = 8;
+            // 
+            // gameBox20
+            // 
+            this.gameBox20.AutoSize = true;
+            this.gameBox20.Font = new System.Drawing.Font("NCAA Clemson Paw Hammer", 63F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameBox20.Location = new System.Drawing.Point(112, 301);
+            this.gameBox20.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.gameBox20.Name = "gameBox20";
+            this.gameBox20.Size = new System.Drawing.Size(0, 70);
+            this.gameBox20.TabIndex = 9;
+            // 
+            // gameBox22
+            // 
+            this.gameBox22.AutoSize = true;
+            this.gameBox22.Font = new System.Drawing.Font("NCAA Clemson Paw Hammer", 63F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameBox22.Location = new System.Drawing.Point(240, 301);
+            this.gameBox22.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.gameBox22.Name = "gameBox22";
+            this.gameBox22.Size = new System.Drawing.Size(0, 70);
+            this.gameBox22.TabIndex = 10;
+            // 
+            // gameBox23
+            // 
+            this.gameBox23.AutoSize = true;
+            this.gameBox23.Font = new System.Drawing.Font("NCAA Clemson Paw Hammer", 63F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameBox23.Location = new System.Drawing.Point(376, 301);
+            this.gameBox23.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.gameBox23.Name = "gameBox23";
+            this.gameBox23.Size = new System.Drawing.Size(0, 70);
+            this.gameBox23.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(505, 414);
+            this.Controls.Add(this.gameBox23);
+            this.Controls.Add(this.gameBox22);
+            this.Controls.Add(this.gameBox20);
+            this.Controls.Add(this.gameBox12);
+            this.Controls.Add(this.gameBox11);
+            this.Controls.Add(this.gameBox10);
+            this.Controls.Add(this.gameBox03);
+            this.Controls.Add(this.gameBox01);
+            this.Controls.Add(this.gameBox00);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.startTurn);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Tick-Tac-Toe";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button startTurn;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Label gameBox00;
+        private System.Windows.Forms.Label gameBox01;
+        private System.Windows.Forms.Label gameBox03;
+        private System.Windows.Forms.Label gameBox10;
+        private System.Windows.Forms.Label gameBox11;
+        private System.Windows.Forms.Label gameBox12;
+        private System.Windows.Forms.Label gameBox20;
+        private System.Windows.Forms.Label gameBox22;
+        private System.Windows.Forms.Label gameBox23;
+    }
+}
+
---
+++ Week_6_Activity_13/Week_6_Activity_13/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_6_Activity_13
+{
+    public partial class Form1 : Form
+
+
+    {
+        // Declare a object for 2D
+        int[,] gamemat = new int[3, 3];
+
+        string winner = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startTurn_Click(object sender, EventArgs e)
+        {
+
+            //Random and loop to add to 2D array
+            Random random = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int numLoop = random.Next(0, 2);
+
+                    gamemat[i, j] = numLoop;
+                }
+            }
+
+
+            //Assigning values to game mat
+            if (gamemat[0, 0] == 0)
+            {
+                gameBox00.Text = "0";
+            }
+            else  
+            {
+                gameBox00.Text = "X";
+            }
+             if (gamemat[0, 1] == 0)
+            {
+                gameBox01.Text = "0";
+            }
+            else 
+            {
+                gameBox01.Text = "X";
+            }
+             if (gamemat[0, 2] == 0)
+            {
+                gameBox03.Text = "0";
+            }
+            else 
+            {
+                gameBox03.Text = "X";
+            }
+            if (gamemat[1, 0] == 0)
+            {
+                gameBox10.Text = "0";
+            }
+            else 
+            {
+                gameBox10.Text = "X";
+            }
+            if (gamemat[1, 1] == 0)
+            {
+                gameBox11.Text = "0";
+            }
+            else 
+            {
+                gameBox11.Text = "X";
+            }
+            if (gamemat[1, 2] == 0)
+            {
+                gameBox12.Text = "0";
+            }
+            else 
+            {
+                gameBox12.Text = "X";
+            }
+            if (gamemat[2, 0] == 0)
+            {
+                gameBox20.Text = "0";
+            }
+            else 
+            {
+                gameBox20.Text = "X";
+            }
+            if (gamemat[2, 1] == 0)
+            {
+                gameBox22.Text = "0";
+            }
+            else 
+            {
+                gameBox22.Text = "X";
+            }
+            if (gamemat[2, 2] == 0)
+            {
+                gameBox23.Text = "0";
+            }
+            else 
+            {
+                gameBox23.Text = "X";
+            }
+
+            if (winner == "")
+            {
+                MessageBox.Show("The Game is a Tie");
+            }
+            else
+                MessageBox.Show(winner + " Wins");
+
+            winner = check_for_winner();
+
+        }
+        //Method to check winner
+        private string check_for_winner()
+        {
+            string winner = "";
+
+            //If O wins
+
+            if (gamemat[0,0] == 0 && gamemat[0,1] == 0 && gamemat[0,2] == 0)
+            {
+                winner = "O";
+            }
+            else if (gamemat[1, 0] == 0 && gamemat[1, 1] == 0 && gamemat[1, 2] == 0)
+            {
+                winner = "O";
+            }
+            else if (gamemat[2, 0] == 0 && gamemat[2, 1] == 0 && gamemat[2, 2] == 0)
+            {
+                winner = "O";
+            }
+            else if (gamemat[0, 0] == 0 && gamemat[1, 1] == 0 && gamemat[2,2] == 0)
+            {
+                winner = "O";
+            }
+            else if (gamemat[0,2] == 0 && gamemat[1,1] == 0 && gamemat[2,0]== 0)
+            {
+                winner = "O";
+            }
+            else if (gamemat[0,0] == 0 && gamemat[1,0] == 0 && gamemat[2,0]== 0)
+            {
+                winner = "O";   
+            }
+            else if (gamemat[0,1] == 0 && gamemat[1,1] == 0 && gamemat[2,1] == 0)
+            {
+                winner= "O";
+            }
+            else if (gamemat[0,2] == 0 && gamemat[1,2]== 0 && gamemat[2,2] == 0)
+            {
+                winner = "O";
+            }
+
+
+
+            //If X wins
+
+            if (gamemat[0, 0] == 1 && gamemat[0, 1] == 1 && gamemat[0, 2] == 1)
+            {
+                winner = "X";
+            }
+            else if (gamemat[1, 0] == 1 && gamemat[1, 1] == 1 && gamemat[1, 2] == 1)
+            {
+                winner = "X";
+            }
+            else if (gamemat[2, 0] == 1 && gamemat[2, 1] == 1 && gamemat[2, 2] == 1)
+            {
+                winner = "X";
+            }
+            else if (gamemat[0, 0] == 1 && gamemat[1, 0] == 1 && gamemat[2, 0] == 1)
+            {
+                winner = "X";
+            }
+            else if (gamemat[0, 1] == 1 && gamemat[1, 1] == 1 && gamemat[2, 1] == 1)
+            {
+                winner = "X";
+            }
+            else if (gamemat[0, 2] == 1 && gamemat[1, 2] == 1 && gamemat[2, 2] == 1)
+            {
+                winner = "X";
+            }
+            else if (gamemat[0, 0] == 1 && gamemat[1, 1] == 1 && gamemat[2, 2] == 1)
+            {
+                winner = "X";
+            }
+            else if (gamemat[0, 2] == 1 && gamemat[1, 1] == 1 && gamemat[2, 0] == 1)
+            {
+                winner = "X";
+            }
+
+
+            return winner;
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
